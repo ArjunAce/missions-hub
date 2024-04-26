7fe166cf07c52a5420e027ecec4dcabeb63fdc7b
arjunace <arjun.mudhaliyar@gmail.com>
2024-04-26 20:32:02 +0200

    Added react-router-dom and dummy Home and About comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import RoutesConfig from "./config/routes.config";
 import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <NavigationBar />
+      <section className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path={RoutesConfig.home} element={<Home />} />
+            <Route path={RoutesConfig.about} element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </section>
     </>
   );
 }
