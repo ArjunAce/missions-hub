ede8a784d15f330e9073dd310ff115f43ba3be5a
arjunace <arjun.mudhaliyar@gmail.com>
2024-04-29 17:14:04 +0200

    Updating default route

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -7,12 +7,12 @@ const NavigationBar = props => {
   return (
     <StyledNavBar>
       <div className="container navigation-container">
-        <Link className="logo" to={RoutesConfig.home}>
+        <Link className="logo" to={RoutesConfig.missions}>
           <img src={logo} alt="Missions Hub Logo" />
           <h1>missions-hub</h1>
         </Link>
         <nav role="navigation" className="nav-menu">
-          <Link to={RoutesConfig.home}>Home</Link>
+          <Link to={RoutesConfig.missions}>Home</Link>
           <Link to={RoutesConfig.about}>About</Link>
         </nav>
       </div>
