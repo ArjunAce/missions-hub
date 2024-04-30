460c2290098749b1c5991c66c6bbf5bf9e78fef0
arjunace <arjun.mudhaliyar@gmail.com>
2024-04-30 15:50:11 +0200

    Making the status filter dynamic

--- src/components/Filters/StatusFilter.tsx
+++ src/components/Filters/StatusFilter.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
 import styled from "styled-components";
+import { MISSION_STATUS_LABELS } from "../../config/constants";
+import { encodeParams, getURLParams } from "../../utils";
 
 const StyledStatusFilter = styled.div`
   .filter-name {
@@ -14,29 +18,50 @@ const StyledStatusFilter = styled.div`
   }
 `;
 
+const status = Object.entries(MISSION_STATUS_LABELS).reduce((acc: { [key: string]: boolean }, [key]) => {
+  acc[key] = false;
+  return acc;
+}, {});
+
 const StatusFilter = () => {
+  const [statusFilters, updateStatusFilters] = useState(status);
+  const navigate = useNavigate();
+  const { pathname, search } = useLocation();
+
+  useEffect(() => {
+    // Debouncing URL update
+    const timerId = setTimeout(() => {
+      const params = getURLParams(new URLSearchParams(search));
+      params.filters = params.filters || {};
+      params.filters.status = statusFilters;
+      const newUrl = pathname + encodeParams(params);
+      navigate(newUrl);
+    }, 500);
+    return () => clearTimeout(timerId);
+  }, [statusFilters]);
+
   return (
     <StyledStatusFilter>
       <div className="filter-name">Status</div>
       <div className="statuses">
-        <div className="status">
-          <label className="status-label" htmlFor="in_progress">
-            In progress
-          </label>
-          <input type="checkbox" id="in_progress" />
-        </div>
-        <div className="status">
-          <label className="status-label" htmlFor="upcoming">
-            Upcoming
-          </label>
-          <input type="checkbox" id="upcoming" />
-        </div>
-        <div className="status">
-          <label className="status-label" htmlFor="completed">
-            Completed
-          </label>
-          <input type="checkbox" id="completed" />
-        </div>
+        {Object.entries(MISSION_STATUS_LABELS).map(([value, label]) => (
+          <div className="status" key={value}>
+            <label className="status-label" htmlFor={value}>
+              {label}
+            </label>
+            <input
+              type="checkbox"
+              id={value}
+              checked={statusFilters[value]}
+              onChange={e => {
+                updateStatusFilters({
+                  ...statusFilters,
+                  [value]: e.target.checked,
+                });
+              }}
+            />
+          </div>
+        ))}
       </div>
     </StyledStatusFilter>
   );
